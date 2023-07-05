8ac8699c49d09c46d7e2a9a9961e3e9f729e885e
Shibin Shibu <shibinsb01@gmail.com>
2023-07-05 21:37:00 +0530

    correction

--- factorial.c
+++ factorial.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 void main(){
-    int number,numTemp, factorial=1,ch;
+    int number,factorial,ch;
     do{
         printf("Enter a number to find the factorial: \n");
         scanf("%d", &number);
-        numTemp=number;
-        for (int i = 0; i < number; i++){
-            factorial*=numTemp;
-            numTemp--;
+        factorial=1;
+        for (int i = 1; i <= number; i++){
+            factorial*=i;
         }
         printf("The factorial of %d is %d\n", number, factorial);
         printf("0 to exit.\nPress any number to calculate again");
