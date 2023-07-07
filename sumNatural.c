0503f8a61f7d5094f8e42ecb258993c6b859ab03
Shibin Shibu <shibinsb01@gmail.com>
2023-07-07 11:30:30 +0530

    reccursion

--- sumNatural.c
+++ sumNatural.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-int sumNatural(int number);
+int sumNatural(int count,int sum);
 
 void main(){
-    int num;
+    int count,sum=0;
+    printf("Enter count of Natural Numbers: ");
+    scanf("%d",&count);
+    sum=sumNatural(count,sum);
+    printf("The sum of %d Natural Numbers is %d",count,sum);
+}
+
+int sumNatural(int count,int sum){
+    if(count==0){
+        return sum;
+    } else {
+        sum+=count;
+        return sumNatural(count-1,sum);;
+    }
 }
