bb1b396c5dd047d64c0736653342e6f6c7905b2f
Shibin Shibu <shibinsb01@gmail.com>
2023-07-13 06:01:58 +0530

    used %p

--- pointer.c
+++ pointer.c
@@ -3,10 +3,10 @@
     int age=22;
     int *ptr=&age;
     int _age=*ptr;
-    int _age_=ptr;
+    int _age_=(int)ptr;
     printf("age= %d\n",age);
-    printf("ptr= %d\n",ptr);
+    printf("ptr= %p\n",ptr);
     printf("*ptr= %d\n",*ptr);
     printf("_age= %d\n",_age);
-    printf("_age_= %d\n",_age_);
+    printf("_age_= %u\n",_age_);
  }
