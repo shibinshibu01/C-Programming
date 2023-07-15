086c06d6d23ecd5506f6e8c98ac84be1c017dc38
Shibin Shibu <shibinsb01@gmail.com>
2023-07-16 05:02:45 +0530

    formatted

--- gstCal.c
+++ gstCal.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
-int calGST(int clothes[]){
-    int gst;
-    for(int i=0; i<sizeof(clothes); i++){
-        gst=(12*clothes[i])/100;
+
+float calGST(float clothes[], int size) {
+    float gst = 0;
+    for (int i = 0; i < size; i++) {
+        gst += (12 * clothes[i]) / 100;
     }
     return gst;
 }
-int main(){
-    int clothes[100],gst[100];
-    int num,rate=0,gstRate=0;
-    printf("Enter no of items: ");
-    scanf("%d",&num);
-    printf("Enter price of %d Items: ",num);
-    for(int i=0; i<num; i++){
-        scanf("%d",&clothes[i]);
-        gst[i] = calGST(clothes);
-        rate+=clothes[i];
-        gstRate+=gst[i];
+
+int main() {
+    float clothes[100], gst[100];
+    int num;
+    float rate = 0, gstRate = 0;
+
+    printf("Enter the number of items: ");
+    scanf("%d", &num);
+
+    printf("Enter the price of %d items: ", num);
+    for (int i = 0; i < num; i++) {
+        scanf("%f", &clothes[i]);
+        rate += clothes[i];
     }
-    printf("\nThe Total Price = %d(Rate) + %d(GST) = %d",rate,gstRate,rate+gstRate);
+
+    gstRate = calGST(clothes, num);
+
+    printf("\nThe Total Price = %.2f (Rate) + %.2f (GST) = %.2f", rate, gstRate, rate + gstRate);
 
     return 0;
 }
