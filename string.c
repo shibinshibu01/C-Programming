c0b92e24b18c97a0466c0433ab9157128361c720
Shibin Shibu <shibinsb01@gmail.com>
2023-07-17 04:24:26 +0530

    Change

--- string.c
+++ string.c
@@ -12,8 +12,15 @@ int main(){
     // printf("%s%s %s",arr,arr1,arr2);
 
     //2
-    char arr[100];
-    //gets(arr);
-    fgets(arr,100,stdin);
-    puts(arr);
+    // char arr[100];
+    // //gets(arr);
+    // fgets(arr,100,stdin);
+    // puts(arr);
+
+    //3
+    char *str="Shibin";
+    puts(str);
+    str="Shibu";
+    puts(str);
+    return 0;
 }
