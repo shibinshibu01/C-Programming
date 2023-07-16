77393dd6123fadb0484759093b753a77af61e078
Shibin Shibu <shibinsb01@gmail.com>
2023-07-17 04:37:11 +0530

    length

--- string.c
+++ string.c
@@ -18,9 +18,22 @@ int main(){
     // puts(arr);
 
     //3
-    char *str="Shibin";
-    puts(str);
-    str="Shibu";
-    puts(str);
-    return 0;
+    // char *str="Shibin";
+    // puts(str);
+    // str="Shibu";
+    // puts(str);
+    // return 0;
+
+    //4
+    char str[100];
+    int count = 0, i = 0;
+    fgets(str, 100, stdin);
+    while (1) {
+        if (str[i] == '\0' || str[i] == '\n')
+            break;
+        else
+            count++;
+        i++;
+    }
+    printf("%d", count);
 }
