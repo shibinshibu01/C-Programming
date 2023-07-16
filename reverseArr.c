ade05dc77fe1d41d8b5f983337aec84ceac6bd5e
Shibin Shibu <shibinsb01@gmail.com>
2023-07-16 19:04:27 +0530

    less space complex

--- reverseArr.c
+++ reverseArr.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
-
-void main(){
-    int arr[]={1,2,3,4,5},arrRev[5],j=4;
-    for(int i=0;i<5;i++){
-        printf("%d\t",arr[i]);
-    }
-    for(int i=0;i<5;i++){
-        arrRev[j]=arr[i];
-        j--;
-    }
-    printf("\n");
-    for(int i=0;i<5;i++){
-        printf("%d\t",arrRev[i]);
+void printArr(int arr[],int n){
+    for (int i = 0; i < n; i++) {
+        printf("%d\t", arr[i]);
     }
 }
+void swap(int *n, int *m) {
+    int temp;
+    temp = *n;
+    *n = *m;
+    *m = temp;
+}
+int main() {
+    int n,j;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The Original Array:\n");
+    printArr(arr,n);
+
+    j=n-1;
+    for(int i = 0; i < n/2; i++) {
+        swap(&arr[i],&arr[j]);
+        j--;
+    }
+    printf("\nThe New Array:\n");
+    printArr(arr,n);
+
+    return 0;
+}
+
+    // int arr[]={1,2,3,4,5},arrRev[5],j=4;
+    // for(int i=0;i<5;i++){
+    //     printf("%d\t",arr[i]);
+    // }
+    // for(int i=0;i<5;i++){
+    //     arrRev[j]=arr[i];
+    //     j--;
+    // }
+    // printf("\n");
+    // for(int i=0;i<5;i++){
+    //     printf("%d\t",arrRev[i]);
+    // }
